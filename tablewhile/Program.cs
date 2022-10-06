8f98ac406b429ba9d58e9c233b2cf36f7b0e9e07
AkshitaRahangdale <akshitarahangdale@1720gmail.com>
2022-10-06 16:30:49 +0530

    Add project files.

---
+++ tablewhile/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Net.Security;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tablewhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i =; i >= 5; i--)
+            {
+                for (int j = 5; j<=i; j++)
+                {
+                    Console.Write(i+"");
+                }
+                Console.WriteLine();
+            }
+        }
+        
+    }
+}
